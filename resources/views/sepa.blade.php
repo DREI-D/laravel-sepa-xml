b2ab40866eae0d114ad12b949f9eec7caa100199
bluecraank <bluecraank@outlook.de>
2024-06-03 14:23:07 +0200

    Change pain.001.002.03 to pain.001.001.03

--- resources/views/sepa.blade.php
+++ resources/views/sepa.blade.php
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<Document xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.002.03" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:iso:std:iso:20022:tech:xsd:pain.001.002.03 pain.001.002.03.xsd">
+<Document xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.03" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:iso:std:iso:20022:tech:xsd:pain.001.001.03 pain.001.001.03.xsd">
     <CstmrCdtTrfInitn>
         <GrpHdr>
             <MsgId>{{ $messageId }}</MsgId>
